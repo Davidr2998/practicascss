855530fea322b81332fa4ad115134f0ae3acd92b
tmchein <davidr2998@gmail.com>
2020-11-23 16:58:18 -0500

    Interfaz completa lista para ser visualizada en 1280x720

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from "react";
-import './App.css';
+import "./App.css";
 import Invoicesdown from "./Components/InvoicesDown/InvoicesDown.js";
+import VerticalNav from "./Components/VerticalNav/VerticalNav";
+import UserSection from "./Components/UserSection/UserSection";
+import TopInvoices from "./Components/TopInvoices/TopInvoices";
+import Profit from "./Components/Profit/Profit";
 
 function App() {
   return (
-    <div className="App">
-       <Invoicesdown></Invoicesdown>
+    <div className="o-app-main-container">
+      <VerticalNav />
+      <UserSection />
+      <div className="o-left-container">
+        <TopInvoices />
+        <Profit />
+        <Invoicesdown />
+      </div>
     </div>
   );
 }
